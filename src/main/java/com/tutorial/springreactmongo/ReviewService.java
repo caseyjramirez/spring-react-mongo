05deca55c929bdb09bbf3fcf88e65edabfde3a30
caseyjramirez <caseyjramirez5@gmail.com>
2023-02-10 09:55:50 -0600

    Initial commit

---
+++ src/main/java/com/tutorial/springreactmongo/ReviewService.java
@@ -0,0 +1,34 @@
+package com.tutorial.springreactmongo;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public Review createNewReview(String imdbId, String body) {
+        Review review = reviewRepository.insert(new Review(body));
+
+        System.out.println("New Review");
+        System.out.println(review);
+        System.out.println("New Review");
+
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviews").value(review))
+                .first();
+
+        return review;
+    }
+
+}
---
+++ src/test/java/com/tutorial/springreactmongo/SpringReactMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.springreactmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
